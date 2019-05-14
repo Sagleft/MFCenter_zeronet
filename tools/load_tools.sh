45be8692ca7a4a7eaa63a8cfa1fba6f1532a552c
Sagleft <sagleft@inbox.ru>
2019-05-14 20:22:49 +0400

    added antizapret

--- tools/load_tools.sh
+++ tools/load_tools.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/Sagleft/MFCenter-PaperWallet.git paper-wallet/
 git clone https://github.com/Sagleft/MFCoin_web-wallet.git web-wallet/
+git clone https://github.com/Sagleft/blockconfirmationalarm.git blockconfirmationalarm/
+git clone https://github.com/Sagleft/antizapret.git antizapret/
